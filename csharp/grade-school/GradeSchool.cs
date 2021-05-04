1bd4f0d78c33bed3df80adfab68c1a3e115341a1
Jason Fetzer <jasonmfetzer@gmail.com>
2021-05-04 10:43:39 -0500

    worked on Grade School Interface.

--- csharp/grade-school/GradeSchool.cs
+++ csharp/grade-school/GradeSchool.cs
@@ -3,18 +3,42 @@ using System.Collections.Generic;
 
 public class GradeSchool
 {
+    //2 dimensional list
+
+    Dictionary<string, int> openSchool =
+    new Dictionary<string, int>();
     public void Add(string student, int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if(openSchool.ContainsKey(student)) 
+        {
+            throw new ArgumentException(student.ToString() + " already in list.");
+        }
+        else
+        {
+            //openSchool.
+            openSchool.Add(student, grade);
+        }
     }
 
     public IEnumerable<string> Roster()
     {
-        throw new NotImplementedException("You need to implement this function.");
+         
+        List<string> ros = new List<string>();
+        foreach (KeyValuePair<string, int> kvp in openSchool)
+            {
+            ros.Add((kvp.Key, kvp.Value).ToString());
+            }
+            return ros;
+            
     }
 
     public IEnumerable<string> Grade(int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+    List<string> ros = new List<string>();
+        foreach (KeyValuePair<string, int> kvp in openSchool)
+            {
+            ros.Add((kvp.Key, kvp.Value).ToString());
+            }
+            return ros;
     }
 }
